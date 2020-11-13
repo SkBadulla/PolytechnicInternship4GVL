d1baf6aa462b4680f82b92208b91f399d65a1ecf
SkBadulla <skbadulla809@gmail.com>
2020-11-13 12:05:40 +0530

    adding inheritance and packages&modules files

---
+++ Day28(13-11-2020)/package/mymath.py
@@ -0,0 +1,25 @@
+def add(n1, n2):
+    return n1 + n2
+    
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+class Arith:
+    
+    def add(n1, n2):
+        return n1 + n2
+    
+    def sub(n1, n2):
+        return n1 - n2
+
+    def mul(n1, n2):
+        return n1 * n2
+    def exp(b, p):
+        return b**p
+    
+    
+    
+    
